86e3a7d02d6f68beb515057da3b370e30c676857
Pranas <k.pranas@gmail.com>
2010-03-04 23:37:26 +0200

    Started preparing C environment

---
+++ include/stdint.h
@@ -0,0 +1,16 @@
+/*
+
+LP64 data model (used by Linux)
+
+char 8
+short 16
+_int 32
+int 32
+long 64
+long long --
+pointer 64
+
+*/
+
+
+
---
+++ kernel/main.c
@@ -0,0 +1,9 @@
+/*
+
+
+*/
+
+void kernel_entry (void* bootinfo) 
+{
+	
+}
