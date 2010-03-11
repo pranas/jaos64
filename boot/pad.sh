2f328564fca9911a6447a4d6f1293d051a512f11
tomasv <t.varneckas@gmail.com>
2010-03-11 18:39:01 +0200

    Rewrote makefiles

--- boot/pad.sh
+++
@@ -1,4 +0,0 @@
-SIZE1=`du -b ../bin/stage1 | awk '{print $1 }'`
-SIZE2=`du -b ../bin/stage2 | awk '{print $1 }'`
-COUNT=$(( ((1440 * 1024) - $SIZE1 - $SIZE2) / 512 ))
-dd if=/dev/zero of=../bin/pad bs=512 count=$COUNT
