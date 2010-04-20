504c821bec98d761f222690075e0eca00caaf882
Pranas <k.pranas@gmail.com>
2010-04-20 18:28:30 +0300

    Memory manager prints memory map (for debuging purposes)

--- include/bootinfo.h
+++ include/bootinfo.h
@@ -1,3 +1,6 @@
+#ifndef _BOOTINFO_H
+#define _BOOTINFO_H
+
 /*
 
 	bootinfo.h
@@ -19,31 +22,32 @@
 	multiboot info structure passed from boot loader
 */
 
-struct multiboot_info {
-
-	uint32_t	m_flags;
+struct multiboot_info
+{
+	uint32_t m_flags;
 	//uint32_t	m_memoryLo;
 	//uint32_t	m_memoryHi;
-	uint64_t	m_memory;
-	uint32_t	m_bootDevice;
-	uint32_t	m_cmdLine;
-	uint32_t	m_modsCount;
-	uint32_t	m_modsAddr;
-	uint32_t	m_syms0;
-	uint32_t	m_syms1;
-	uint32_t	m_syms2;
-	uint32_t	m_mmap_length;
-	uint32_t	m_mmap_addr;
-	uint32_t	m_drives_length;
-	uint32_t	m_drives_addr;
-	uint32_t	m_config_table;
-	uint32_t	m_bootloader_name;
-	uint32_t	m_apm_table;
-	uint32_t	m_vbe_control_info;
-	uint32_t	m_vbe_mode_info;
-	uint16_t	m_vbe_mode;
-	uint32_t	m_vbe_interface_addr;
-	uint16_t	m_vbe_interface_len;
-};
-
+	uint64_t m_memory;
+	uint32_t m_bootDevice;
+	uint32_t m_cmdLine;
+	uint32_t m_modsCount;
+	uint32_t m_modsAddr;
+	uint32_t m_syms0;
+	uint32_t m_syms1;
+	uint32_t m_syms2;
+	uint32_t m_mmap_length;
+	uint32_t m_mmap_addr;
+	uint32_t m_drives_length;
+	uint32_t m_drives_addr;
+	uint32_t m_config_table;
+	uint32_t m_bootloader_name;
+	uint32_t m_apm_table;
+	uint32_t m_vbe_control_info;
+	uint32_t m_vbe_mode_info;
+	uint16_t m_vbe_mode;
+	uint32_t m_vbe_interface_addr;
+	uint16_t m_vbe_interface_len;
+} __attribute__((packed));
 typedef struct multiboot_info multiboot_info;
+
+#endif
