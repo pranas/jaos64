3c27c6243f5cb0a23cac1e41d40c42faa10d0cb4
tomasv <t.varneckas@gmail.com>
2010-05-04 23:30:50 +0300

    TR loading done, not sure if TSS itself is good.

--- kernel/gdt.c
+++ kernel/gdt.c
@@ -18,27 +18,30 @@ void gdt_set_gate(int num, int32_t base, int32_t limit, int8_t access, int8_t gr
 
 void gdt_set_ss_gate(int num, int64_t base, int32_t limit, int8_t access, int8_t granularity)
 {
-	system_segment_descriptor* ss_gdt = (system_segment_descriptor*) gdt;
 	/* base address */
-	ss_gdt[num].base_low     = (base & 0xFFFF);
-	ss_gdt[num].base_middle  = (base >> 16) & 0xFF;
-	ss_gdt[num].base_high    = (base >> 24) & 0xFF;
-	ss_gdt[num].base_veryhigh = (base >> 32) & 0xFFFF;
+	gdt[num].base_low    = (base & 0xFFFF);
+	gdt[num].base_middle = (base >> 16) & 0xFF;
+	gdt[num].base_high   = (base >> 24) & 0xFF;
 
-	/* limit */
-	ss_gdt[num].limit_low    = (limit & 0xFFFF);
-	ss_gdt[num].granularity  = (limit >> 16) & 0x0F;
+	gdt[num+1].base_low = (base >> 48) & 0xFFFF;
+	gdt[num+1].limit_low = (base >> 32) & 0x0000FFFF;
+	gdt[num+1].base_middle = 0;
+	gdt[num+1].access = 0;
+	gdt[num+1].granularity = 0;
+	gdt[num+1].base_high = 0;
 
-	/* granularity */
-	ss_gdt[num].granularity |= (granularity & 0xF0);
-	ss_gdt[num].access       = access;
+	gdt[num].limit_low    = (limit & 0xFFFF);
+	gdt[num].granularity  = (limit >> 16) & 0x0F;
 
-	ss_gdt[num].zero = 0;
+	gdt[num].granularity |= (granularity & 0xF0);
+	gdt[num].access       = access;
+
 }
 
 void gdt_install()
 {
 	gdt_ptr.limit = (sizeof(gdt_entry_struct) * GDT_ENTRY_NR) - 1;
+	putint(gdt_ptr.limit);
 	gdt_ptr.base  = (int64_t) &gdt;
 
 	// NULL gate
@@ -52,6 +55,31 @@ void gdt_install()
 	gdt_set_gate(3, 0, 0, 0xF8, 0x20); // code
 	gdt_set_gate(4, 0, 0, 0xF2, 0x00); // data
 
-	//apply changes
+	// try to add a tss gate
+	tss_set_gate(5, 0x90000, 0x90000, 0x90000);
+
+	// apply changes
 	gdt_flush();
+
+	// must be after gdt is loaded
+	load_tr(5);
+}
+
+void load_tr(int num)
+{
+	int sel = num * 8;
+	asm ("ltr %%ax" :: "a" (sel));
+}
+
+void tss_set_gate(int num, uint64_t rsp0, uint64_t rsp1, uint64_t rsp2)
+{
+	uint64_t base = (uint64_t) &tss_entry;
+	uint64_t limit = base + sizeof(tss_entry);
+
+	gdt_set_ss_gate(num, base, limit, 0x89, 0x9F);
+	memset(&tss_entry, 0, sizeof(tss_entry));
+
+	tss_entry.rsp0 = rsp0;
+	tss_entry.rsp1 = rsp1;
+	tss_entry.rsp2 = rsp2;
 }
--- kernel/gdt.h
+++ kernel/gdt.h
@@ -16,12 +16,12 @@ typedef struct gdt_entry_struct gdt_entry_struct;
 
 struct system_segment_descriptor
 {
-   int16_t limit_low;           // The lower 16 bits of the limit.
-   int16_t base_low;            // The lower 16 bits of the base.
-   int8_t  base_middle;         // The next 8 bits of the base.
-   int8_t  access;              // Access flags
-   int8_t  granularity;         // with 4 bits of limit
-   int8_t  base_high;           // The last 8 bits of the base.
+   int16_t  limit_low;           // The lower 16 bits of the limit.
+   int16_t  base_low;            // The lower 16 bits of the base.
+   int8_t   base_middle;
+   int8_t   access;              // Access flags
+   int8_t   granularity;
+   int8_t   base_high;
    uint32_t base_veryhigh;
    uint32_t zero;
 } __attribute__((packed));
@@ -35,16 +35,41 @@ struct gdt_ptr_struct
 } __attribute__((packed));
 typedef struct gdt_ptr_struct gdt_ptr_struct;
 
-#define GDT_ENTRY_NR 7
+struct tss_entry_t
+{
+	uint32_t zero1;
+	uint64_t rsp0;
+	uint64_t rsp1;
+	uint64_t rsp2;
+	uint64_t zero2;
+	uint64_t ist0;
+	uint64_t ist1;
+	uint64_t ist2;
+	uint64_t ist3;
+	uint64_t ist4;
+	uint64_t ist5;
+	uint64_t ist6;
+	uint64_t ist7;
+	uint64_t zero3;
+	uint16_t zero4;
+	uint16_t iomap_adr;
+};
+
+typedef struct tss_entry_t tss_entry_t;
+#define GDT_ENTRY_NR 32
 struct gdt_ptr_struct gdt_ptr;
 struct gdt_entry_struct gdt[GDT_ENTRY_NR];
+tss_entry_t tss_entry;
 
 /* this one is in gdt_flush.asm */
 extern void gdt_flush();
 
 void gdt_install();
 void gdt_set_gate(int num, int32_t base, int32_t limit, int8_t access, int8_t granularity);
+// for system segment descriptors (TSS)
 void gdt_set_ss_gate(int num, int64_t base, int32_t limit, int8_t access, int8_t granularity);
+void tss_set_gate(int num, uint64_t rsp0, uint64_t rsp1, uint64_t rsp2);
+void load_tr(int num);
 
 #endif
 
