e49e95b680dd3659df455df1b908da972fd687e2
Pranas <k.pranas@gmail.com>
2010-05-06 00:29:46 +0300

    fixed apic interrupt handling, removed handler message for interrupts with custom handler

--- kernel/isr.c
+++ kernel/isr.c
@@ -6,18 +6,22 @@ isr_t handlers[MAX_HANDLERS];
 
 void isr_handler(registers_t regs)
 {
-	puts("[ISR] INT ");	puthex(regs.int_no);
-	puts(", error code "); puthex(regs.err_code);
-	puts("\n");
+	if (regs.int_no >= 0x20)
+		write_apicr(APIC_BASE, 0xb0, 0); // acknowledge ioapic interrupt
+
 
 	if (handlers[regs.int_no] != 0)
 	{
 		isr_t handler = handlers[regs.int_no];
 		handler(regs);
 	}
+	else
+	{
+		puts("[ISR] INT ");	puthex(regs.int_no);
+		puts(", error code "); puthex(regs.err_code);
+		puts("\n");
+	}
 
-	if (regs.int_no >= 0x20)
-		write_apicr(APIC_BASE, 0xb0, 0); // acknowledge ioapic interrupt
 }
 
 void register_handler(int int_no, isr_t handler)
