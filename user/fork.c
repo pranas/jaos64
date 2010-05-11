584087aab8666d5682f41617fd85e9d49316fe0d
Pranas <k.pranas@gmail.com>
2010-05-11 22:49:50 +0300

    initial user mode fork()

---
+++ user/fork.c
@@ -0,0 +1,24 @@
+#define puts syscall_puts
+#define fork syscall_fork
+
+int main()
+{
+    asm volatile("xchg %bx, %bx");
+	if (fork() == 0)
+	{
+        for(;;)
+        {
+            puts("Child\n");
+            asm volatile("hlt");
+        }
+	}
+	else
+	{
+        for(;;)
+        {
+            puts("Father\n");
+            asm volatile("hlt");
+        }
+	}
+    return 0;
+}
